5d3efbda7047fef57217536772d8caf812bf1604
Remy Lim Wee Meng <81542313+Lim-Wee-Meng@users.noreply.github.com>
2021-05-30 16:41:30 +0800

    Add files via upload

---
+++ SQL_Exercise_Resources/Students.sql
@@ -0,0 +1,16 @@
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1001','Jenny','Lim','jenny@email.com','Ang Mo Kio','1990-01-01',’1’);
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1002','Andy','Lau','a@email.com','some address','1990-01-01',’1’);
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1003','Jacky','Chang','b@email.com','some address','1990-01-01',’1’);
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1004','Leon','Lai','c@email.com','some address','1990-01-01','2');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1005','Aaron','Kwok','d@email.com','some address','1990-01-01','2');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1006','Annie','Lim','e@email.com','some address','1990-01-01','2');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1007','Joan','Lim','f@email.com','some address','1990-01-01','3');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1008','Faith','Yeo','g@email.com','some address','1990-01-01','3');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1009','Sue','Tan','h@email.com','some address','1990-01-01','4');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1010','Joe','Lau','i@email.com','some address','1990-01-01','4');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1011','Jimmy','Lee','j@email.com','some address','1990-01-01','4');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1012','Wendy','Ting','k@email.com','some address','1990-01-01','5');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1013','Jo','Kwok','l@email.com','some address','1990-01-01','5');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1014','Kim','Lee','m@email.com','some address','1990-01-01','6');
+INSERT INTO generation.Students(idStudent,name,last_name,email,address,birthdate,nationality) VALUES ('1015','Tanny','Lau','n@email.com','some address','1990-01-01','6');
+
